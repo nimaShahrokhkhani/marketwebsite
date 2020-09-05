8aef32de479287506caee9e3ab59355560ba48e8
mohammadreza novinzad <mohammadreza.novinzad@gmail.com>
2020-09-05 19:27:47 +0430

    login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './background.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+    }
+
+    login() {
+        alert('hiii')
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="loginForm">
+                    <section id="content">
+                        <form action="">
+                            <h1>Login Form</h1>
+                            <div>
+                                <input type="text" placeholder="Username" required="" id="username"/>
+                            </div>
+                            <div>
+                                <input type="password" placeholder="Password" required="" id="password"/>
+                            </div>
+                            <div>
+                                <button onClick={this.login} >login
+
+                                </button>
+                                <a href="#">Lost your password?</a>
+                                <a href="#">Register</a>
+                            </div>
+                        </form>
+
+                    </section>
+                </div>
+                <div className="imageBackground">
+                    <img src={require('./background.jpg')} width="100%" height="100%"/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
