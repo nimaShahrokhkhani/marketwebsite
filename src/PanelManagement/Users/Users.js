94756bf8e2adf37dca876e0bfc575a8c1fc70037
mohammadreza novinzad <mohammadreza.novinzad@gmail.com>
2020-09-09 14:31:14 +0430

    Add Login

---
+++ src/PanelManagement/Users/Users.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Users.css';
+
+class Users extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    welcome
+                </p>
+            </div>
+
+
+        );
+    }
+}
+
+export default Users;
