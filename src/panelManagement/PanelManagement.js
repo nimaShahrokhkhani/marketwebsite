0e88c36b9e090eccc94f55778ddc8009da62ffee
mohammadreza novinzad <mohammadreza.novinzad@gmail.com>
2020-09-09 13:43:42 +0430

    Add Login

---
+++ src/panelManagement/PanelManagement.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './PanelManagement.css';
+
+class PanelManagement extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    welcome
+                </p>
+            </div>
+
+
+    );
+    }
+}
+
+export default PanelManagement;
